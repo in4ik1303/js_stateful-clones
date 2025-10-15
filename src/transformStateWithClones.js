6c6e94d8eab4fc4907f136c13c38bc548f64c8de
Inna Nedohybchenko <inna_roeva@ukr.net>
2025-10-15 14:31:48 +0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-const-assign */
 'use strict';
 
 /**
@@ -7,7 +8,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const resultState = { ...state };
+  const historyState = [];
+
+  for (const action of actions) {
+    let newState = { ...resultState };
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      newState = { ...newState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+    historyState.push(newState);
+    resultState = newState;
+  }
+
+  return historyState;
 }
 
 module.exports = transformStateWithClones;
